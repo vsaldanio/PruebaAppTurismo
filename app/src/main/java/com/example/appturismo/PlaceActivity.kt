e1699ed4933aa71bf572c8867e9d817bfa23f2a1
vsaldanio <vsaldanio@gmail.com>
2023-05-22 10:49:27 -0300

    Primer commit

---
+++ app/src/main/java/com/example/appturismo/PlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appturismo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place)
+    }
+}
